84f60c63339c4fa672e6a40a2730683771855526
kalizink <kalizink65@gmail.com>
2024-02-01 22:36:46 -0600

    Added NavbarComp

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
 import HomepageProjects from "./HomepageProjects";
+import NavbarComp from "./NavbarComp";
 
 export default function App() {
 	return (
 		<div className="App">
 			<div className="container">
+				<NavbarComp />
 				<Header />
 				<br />
 				<hr />
