bf3ad8764342ea1cb03cd380560b242fb528e1e3
kalizink <kalizink65@gmail.com>
2024-02-01 22:49:57 -0600

    Styled NavbarComp

--- src/NavbarComp.js
+++ src/NavbarComp.js
@@ -2,29 +2,19 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
-function BasicExample() {
+function NavbarComp() {
 	return (
-		<Navbar expand="lg" className="bg-body-tertiary">
+		<Navbar bg="dark" variant="dark" fixed="top" expand="lg">
 			<Container>
-				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+				<Navbar.Brand href="#home">Kali Zink</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
 						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#link">Link</Nav.Link>
-						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.2">
-								Another action
-							</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action/3.4">
-								Separated link
-							</NavDropdown.Item>
-						</NavDropdown>
+						<Nav.Link href="#about">About</Nav.Link>{" "}
+						<Nav.Link href="#about">Work</Nav.Link>
+						<Nav.Link href="#about">Contact</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
@@ -32,4 +22,4 @@ function BasicExample() {
 	);
 }
 
-export default BasicExample;
+export default NavbarComp;
