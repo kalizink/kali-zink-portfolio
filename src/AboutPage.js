d9347269f5c46b3e7724c7b02a2fb077bd6b875d
kalizink <kalizink65@gmail.com>
2024-02-02 11:14:46 -0600

    Made Navbar active

---
+++ src/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavbarComp from "./NavbarComp";
+import { Nav } from "react-bootstrap";
+
+export default function AboutPage() {
+	return (
+		<div>
+			<NavbarComp />
+			<br />
+			<div className="mt-5">About Page!!!</div>
+		</div>
+	);
+}
--- src/NavbarComp.js
+++ src/NavbarComp.js
@@ -2,19 +2,43 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import "./NavbarComp.css";
 
 function NavbarComp() {
 	return (
 		<Navbar bg="dark" variant="dark" fixed="top" expand="lg">
 			<Container>
-				<Navbar.Brand href="#home">Kali Zink</Navbar.Brand>
+				<Navbar.Brand>
+					<Link to="/" className="link">
+						Kali Zink
+					</Link>
+				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#about">About</Nav.Link>{" "}
-						<Nav.Link href="#about">Work</Nav.Link>
-						<Nav.Link href="#about">Contact</Nav.Link>
+					<Nav className="me-auto ">
+						<Nav>
+							<Link to="/" className="link">
+								Home
+							</Link>
+						</Nav>
+						{/* messing around here */}
+						<Nav>
+							<Link to="/AboutPage" className="link">
+								About
+							</Link>
+						</Nav>
+						{/* messing around here */}
+						<Nav>
+							<Link to="/WorkPage" className="link">
+								Work
+							</Link>
+						</Nav>
+						<Nav>
+							<Link to="/ContactPage" className="link">
+								Contact
+							</Link>
+						</Nav>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
