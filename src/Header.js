01aec176b1997d6893453fe040f0d8d1d44cfd08
kalizink <kalizink65@gmail.com>
2024-02-01 21:01:01 -0600

    Fixed bugs to delpoy

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
 			{/* navbar start */}
 			<nav className="navbar navbar-expand-lg navbar-light bg-light">
 				<a className="navbar-brand" href="/">
-					<img src={tiger} className="img-fluid" />
+					<img src={tiger} className="img-fluid" alt="Chinese zodiac" />
 				</a>
 				<button
 					className="navbar-toggler"
@@ -23,22 +23,22 @@ export default function Header() {
 				<div className="collapse navbar-collapse" id="navbarNav">
 					<ul className="navbar-nav">
 						<li className="nav-item active">
-							<a className="nav-link" href="#">
+							<a className="nav-link" href="/">
 								Home <span className="sr-only">(current)</span>
 							</a>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="#">
+							<a className="nav-link" href="/">
 								Features
 							</a>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="#">
+							<a className="nav-link" href="/">
 								Pricing
 							</a>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link disabled" href="#">
+							<a className="nav-link disabled" href="/">
 								Disabled
 							</a>
 						</li>
