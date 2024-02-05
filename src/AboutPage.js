54fd3c3da535ed85f28b954033aaf65a7f4894be
kalizink <kalizink65@gmail.com>
2024-02-05 12:26:22 -0600

    Added hobbies content to AboutPage

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -2,6 +2,12 @@ import React from "react";
 import NavbarComp from "./NavbarComp";
 import profileImg from "./profile.jpg";
 import "./AboutPage.css";
+import gym from "./gym.png";
+import cat from "./cat.png";
+import meditation from "./meditation.png";
+import food from "./food.png";
+import books from "./books.png";
+import plants from "./plants.png";
 
 export default function AboutPage() {
 	return (
@@ -32,14 +38,56 @@ export default function AboutPage() {
 			<section>
 				<div className="row">
 					<div className="col grid">
-						<div className="box">img1</div>
-						<div className="box">img2</div>
-						<div className="box">img3</div>
-						<div className="box">img4</div>
-						<div className="box">img5</div>
-						<div className="box">img6</div>
+						<div className="box">
+							<img
+								src={gym}
+								alt="gym"
+								title="working out"
+								className="img-fluid"
+							/>
+						</div>
+						<div className="box">
+							<img src={cat} alt="cat" title="cats" className="img-fluid" />
+						</div>
+						<div className="box">
+							<img
+								src={meditation}
+								alt="meditation"
+								title="meditation"
+								className="img-fluid"
+							/>
+						</div>
+						<div className="box">
+							<img
+								src={food}
+								alt="food"
+								title="cooking"
+								className="img-fluid"
+							/>
+						</div>
+						<div className="box">
+							<img
+								src={books}
+								alt="books"
+								title="books"
+								className="img-fluid"
+							/>
+						</div>
+						<div className="box">
+							<img
+								src={plants}
+								alt="plants"
+								title="plants"
+								className="img-fluid"
+							/>
+						</div>
 					</div>
-					<div className="col">Hobbies:</div>
+					<div className="col text-center text">
+						<h3>My Hobbies:</h3>
+						<p className="mt-4">
+							{`I like to invest in myself and continue to grow by meditating and reading thriller or self-help books. I also go to the gym consistently and meal prep fun, healthy meals because I found a new interest in cooking. I enjoy the process even though it doesn’t always come out as expected (haha). I love my plants and spending time with my two cats. `}
+						</p>
+					</div>
 				</div>
 			</section>
 		</div>
