7e978700c8657a29be28d7ae0f54902e61f45c98
kalizink <kalizink65@gmail.com>
2024-02-05 09:56:11 -0600

    Added content to about page and fixed bugs for deploy

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,12 +1,32 @@
 import React from "react";
 import NavbarComp from "./NavbarComp";
+import profileImg from "./profile.jpg";
+import "./AboutPage.css";
 
 export default function AboutPage() {
 	return (
-		<div>
+		<div className="AboutPage">
 			<NavbarComp />
 			<br />
-			<div className="mt-5">About Page!!!</div>
+			<div className="mt-5">
+				<div className="row">
+					<div className="col">
+						<h1 className="text-center">Kali Zink</h1>
+						<h2 className="text-center">Jr. React Front-end Developer</h2>
+						<div>
+							{`I'm an eager, self-starting individual who possesses the technical proficiency to debug, optimize functions, learn new platforms, and elevate user experience. Seeking to leverage technical skillset, the opportunity to collaborate, and grow professionally.`}
+						</div>
+					</div>
+					<div className="col">
+						<img
+							src={profileImg}
+							alt="profile pic"
+							title="Kali Zink"
+							className="img-fluid "
+						/>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
