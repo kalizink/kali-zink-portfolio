a06e97ed11f406cc24ef4c2ecef9b67775554fdc
kalizink <kalizink65@gmail.com>
2024-02-05 12:37:54 -0600

    AboutPage: CSS edits and responsive

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -16,22 +16,22 @@ export default function AboutPage() {
 		<div className="AboutPage">
 			<NavbarComp />
 			<br />
-			<section className="mt-5">
+			<section className="mt-5 header">
 				<div className="row">
 					<div className="col mt-5">
-						<h1 className="text-center">Kali Zink</h1>
+						<h1 className="text-center mt-5">Kali Zink</h1>
 						<h2 className="text-center mt-3">Jr. React Front-end Developer</h2>
 						<div className="mt-4">
 							{`I'm an eager, self-starting individual who possesses the technical proficiency to debug, optimize functions, learn new platforms, and elevate user experience. Seeking to leverage technical skillset, the opportunity to collaborate, and grow professionally.`}
 						</div>
 					</div>
-					<div className="col img-container">
+					<div className="col img-container text-center">
 						{" "}
 						<img
 							src={profileImg}
 							alt="profile pic"
 							title="Kali Zink"
-							className="img-fluid "
+							className="img-fluid profile"
 						/>
 					</div>
 				</div>
