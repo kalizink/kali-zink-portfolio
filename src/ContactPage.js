bf5b544f494a4f5f340c66038fbb5dff57acb473
kalizink <kalizink65@gmail.com>
2024-02-05 20:09:54 -0600

    WorkPage: added Footer and WorkInquiry

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -4,6 +4,8 @@ import "./ContactPage.css";
 import email from "./email.png";
 import github from "./github.png";
 import linkedIn from "./linkedIn.png";
+import WorkInquiry from "./WorkInquiry";
+import Footer from "./Footer";
 
 export default function ContactPage() {
 	return (
@@ -13,7 +15,6 @@ export default function ContactPage() {
 			<div className="body mt-5">
 				<h1 className="text-center">{`Let's Get in Touch!`}</h1>
 			</div>
-
 			<div className="text-center mt-5">
 				<img
 					src={email}
@@ -26,12 +27,12 @@ export default function ContactPage() {
 						href="mailto:kalizink65@gmail.com"
 						alt="email"
 						title="kalizink65@gmail.com"
-						target="blank">
+						target="blank"
+						className="text">
 						kalizink65@gmail.com
 					</a>
 				</h2>
 			</div>
-
 			<div className="text-center mt-5">
 				<img src={github} alt="github" title="GitHub" />
 				<h2 className="mt-3">
@@ -39,13 +40,12 @@ export default function ContactPage() {
 						href="https://github.com/kalizink"
 						alt="github"
 						title="GitHub"
-						target="blank">
-						{" "}
+						target="blank"
+						className="text">
 						https://github.com/kalizink
 					</a>
 				</h2>
 			</div>
-			{/* *** */}
 			<div className="text-center mt-3">
 				<img src={linkedIn} alt="linkedIn" title="LinkedIn" />
 				<h2>
@@ -53,13 +53,16 @@ export default function ContactPage() {
 						href="https://www.linkedin.com/in/kalizink/"
 						alt="linkedIn"
 						title="LinkedIn Profile"
-						target="blank">
-						{" "}
+						target="blank"
+						className="text">
 						https://www.linkedin.com/in/kalizink/
 					</a>
 				</h2>
 			</div>
-			{/* *** */}
+			<br />
+			<WorkInquiry />
+			<br />
+			<Footer />
 		</div>
 	);
 }
