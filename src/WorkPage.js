f61e84778a624669331b5ba63d8dd7459b315eeb
kalizink <kalizink65@gmail.com>
2024-02-05 19:20:26 -0600

    WorkPage responsive edits

--- src/WorkPage.js
+++ src/WorkPage.js
@@ -16,14 +16,26 @@ export default function WorkPage() {
 				<strong>Tools and Equipment:</strong> VS Code, MacOS, Github, and
 				Netlify{" "}
 			</p>
-			<br />
-			<section>
+
+			<section className="margin">
 				<div className="row">
 					<div className="column">
-						<img src={dictionary} alt="dictionary" title="dictionary app" />
+						<div className="animateImg">
+							<a
+								href="https://dictionary-kz.netlify.app/"
+								alt="dictionary app"
+								title="Dictionary App"
+								target="blank">
+								<img src={dictionary} alt="Dictionary" title="Dictionary App" />
+							</a>
+							<div className="text-center description">
+								<strong>Dictionary App</strong> built with: React.js,
+								JavaScript, API, and Bootstrap
+							</div>
+						</div>
 					</div>
+
 					<div className="column">
-						{/* *** */}
 						<div className="animateImg">
 							<a
 								href="https://weather-react-app-kz.netlify.app/"
@@ -37,9 +49,38 @@ export default function WorkPage() {
 								and Bootstrap
 							</div>
 						</div>
-						{/* *** */}
-						<img src={aiPoem} alt="AI Generator" title="AI Generator App" />
-						<img src={boogie} alt="boogie" title="Responsive app" />
+
+						<div className="animateImg">
+							<a
+								href="https://ai-poem-generator-kz.netlify.app/"
+								alt="AI Generator"
+								title="AI Generator"
+								target="blank">
+								<img src={aiPoem} alt="weather" title="AI Generator" />
+							</a>
+							<div className="text-center description">
+								<strong>AI Poem Generator</strong> built with: JavaScript, API,
+								and Artificial Intelligence{" "}
+							</div>
+						</div>
+
+						<div className="animateImg">
+							<a
+								href="https://a-boogie-responsive-website-kz.netlify.app/"
+								alt="Responsive Web Design"
+								title="Responsive Web Design"
+								target="blank">
+								<img
+									src={boogie}
+									alt="Responsive Web Design"
+									title="Responsive Web Design"
+								/>
+							</a>
+							<div className="text-center description">
+								<strong>Responsive Web Design</strong> built with: Flexbox,
+								Google SEO, and Responsive
+							</div>
+						</div>
 					</div>
 				</div>
 			</section>
