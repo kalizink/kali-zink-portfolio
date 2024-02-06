cacf43188f2d7054e62c4d2282b8c40ce55e8276
kalizink <kalizink65@gmail.com>
2024-02-05 20:04:59 -0600

    Added links to ContactPage

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -13,6 +13,7 @@ export default function ContactPage() {
 			<div className="body mt-5">
 				<h1 className="text-center">{`Let's Get in Touch!`}</h1>
 			</div>
+
 			<div className="text-center mt-5">
 				<img
 					src={email}
@@ -20,21 +21,45 @@ export default function ContactPage() {
 					title="kalizink65@gmail.com"
 					className="text-center"
 				/>
-				<h2 className="mt-3">kalizink65@gmail.com</h2>
+				<h2 className="mt-3">
+					<a
+						href="mailto:kalizink65@gmail.com"
+						alt="email"
+						title="kalizink65@gmail.com"
+						target="blank">
+						kalizink65@gmail.com
+					</a>
+				</h2>
 			</div>
+
 			<div className="text-center mt-5">
 				<img src={github} alt="github" title="GitHub" />
-				<h2 className="mt-3">https://github.com/kalizink</h2>
+				<h2 className="mt-3">
+					<a
+						href="https://github.com/kalizink"
+						alt="github"
+						title="GitHub"
+						target="blank">
+						{" "}
+						https://github.com/kalizink
+					</a>
+				</h2>
 			</div>
+			{/* *** */}
 			<div className="text-center mt-3">
-				<img
-					src={linkedIn}
-					alt="linkedIn"
-					title="LinkedIn"
-					
-				/>
-				<h2 >https://www.linkedin.com/in/kalizink/</h2>
+				<img src={linkedIn} alt="linkedIn" title="LinkedIn" />
+				<h2>
+					<a
+						href="https://www.linkedin.com/in/kalizink/"
+						alt="linkedIn"
+						title="LinkedIn Profile"
+						target="blank">
+						{" "}
+						https://www.linkedin.com/in/kalizink/
+					</a>
+				</h2>
 			</div>
+			{/* *** */}
 		</div>
 	);
 }
